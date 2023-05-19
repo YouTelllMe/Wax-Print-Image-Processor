3dba7733e35accfc8d7c08e4e09704fe38a7ccdb
Marcus Lai <marcus41881653@gmail.com>
2023-05-19 23:58:32 +0800

    changes

--- ForMarcusGettingStarted/HelperFunctions/rotateMat.m
+++
@@ -1,6 +0,0 @@
-% Returns a rotation matrix to rotate a 2d vector counterclockwise by the
-% theta argument in radians.
-
-function rotMat = rotateMat(theta)
-     rotMat = [cos(theta), -sin(theta); sin(theta), cos(theta)];
-end
--- ForMarcusGettingStarted/Matlab - linear conversion/rotateMatDeg.m
+++
@@ -1,6 +0,0 @@
-% Returns a rotation matrix to rotate a 2d vector, which rotates by the
-% theta argument in degrees.
-
-function rotMat = rotateMatDeg(theta)
-     rotMat = [cosd(theta), -sind(theta); sind(theta), cosd(theta)];
-end
